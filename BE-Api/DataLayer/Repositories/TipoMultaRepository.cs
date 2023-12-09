3121d25a171f1e0d5564ae1e9a51759a609099ac
Rainiery <rainierypenajrg@gmail.com>
2023-12-09 16:44:19 -0400

    TipoMulta repository

---
+++ BE-Api/DataLayer/Repositories/TipoMultaRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE.FinalAmadis.DataLayer.Context;
+using BE.FinalAmadis.DataLayer.Core;
+using BE.FinalAmadis.DataLayer.Entities;
+using BE.FinalAmadis.DataLayer.Interfaces;
+
+namespace BE.FinalAmadis.DataLayer.Repositories
+{
+    public class TipoMultaRepository : Repository<TipoMulta>,ITipoMultaRepository
+
+    {
+        private readonly ProyectoContext _context;
+        public TipoMultaRepository(ProyectoContext context) : base(context) { }
+        
+            
+        
+
+
+    }
+}
